db17c3a67544437605a6ba77c1896317aa363a1d
Anton <tony.smirno@yandex.ru>
2017-12-05 15:27:16 +0300

    created earch for elem in scr

---
+++ csr_to_matrix.py
@@ -0,0 +1,16 @@
+def matrix(alem,jptr,iptr):
+    """int procedure(i,j)
+        {
+            AA=0; // значение искомого элемента
+            N1=LI[i];
+            N2=LI[i+1];
+            for(k=N1;k<N2;k++)
+            {
+                if (LJ[k]==j)
+                {
+                    AA=A[k];
+                    <break>;
+                }
+            }
+            return AA;
+        }"""
